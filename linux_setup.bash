0dce51393df62f16d22059006a992de1e261f142
sammtcbn <>
2020-06-14 23:23:01 +0800

    change default installed path

--- linux_setup.bash
+++ linux_setup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-#INSPATH=~/bin/
-INSPATH=/usr/local/bin/
+INSPATH=~/bin/
+#INSPATH=/usr/local/bin/
 mkdir -p ${INSPATH} || exit 1
 cp -f lineto ${INSPATH} || exit 1
 chmod +x ${INSPATH}/lineto || exit 1
